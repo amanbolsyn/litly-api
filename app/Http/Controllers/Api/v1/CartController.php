45b2c140fc5e3c3865bb52361071bfcec6e7f30d
Aman Bolsyn <nurgozhiyevaman@gmail.com>
2026-03-22 02:43:56 +0500

    feat: defined api routes

---
+++ app/Http/Controllers/Api/v1/CartController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+
+use App\Models\Cart;
+use Illuminate\Http\Request;
+
+class CartController
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Cart $cart)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Cart $cart)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Cart $cart)
+    {
+        //
+    }
+}
